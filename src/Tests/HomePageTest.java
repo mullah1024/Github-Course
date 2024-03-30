5ae9ff8ca3d4835947921e2c0ae990ff848e0c63
mullah1024 <mohammed.ullah1@gmail.com>
2024-03-30 04:14:53 -0400

    first commit

---
+++ src/Tests/HomePageTest.java
@@ -0,0 +1,8 @@
+package Tests;
+
+public class HomePageTest {
+
+    public void loginTest(){
+        System.out.println("Hi this is my home page test");
+    }
+}
---
+++ src/Tests/LoginPageTest.java
@@ -0,0 +1,8 @@
+package Tests;
+
+public class LoginPageTest {
+
+    public void loginTest(){
+        System.out.println("Hi this is my login test");
+    }
+}
